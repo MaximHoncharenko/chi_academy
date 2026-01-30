3995b8fb974f213eb3f55d40513486fbc2d86acd
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2026-01-30 18:40:58 +0200

    Add About page component with introductory content

---
+++ H_W_6/my-next-app1/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Welcome to the About page!</h1>
+      <p>This page is created using the App Router in Next.js 13.</p>
+    </div>
+  );
+}
